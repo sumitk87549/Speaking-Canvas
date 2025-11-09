7ef40d7dcfe464d55c5250d44ed0575b0fecc762
Sumit Kumar <sumitk87549@gmail.com>
2025-11-09 15:23:21 +0530

    speaking canvas init

---
+++ src/main/java/com/gallery/SpeakingCanvas/SpeakingCanvasApplication.java
@@ -0,0 +1,13 @@
+package com.gallery.SpeakingCanvas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeakingCanvasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeakingCanvasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gallery/SpeakingCanvas/SpeakingCanvasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gallery.SpeakingCanvas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeakingCanvasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
